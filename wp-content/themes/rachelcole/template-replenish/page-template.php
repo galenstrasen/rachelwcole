c661b8b8bc0b818a9315a15bdf9d9a23c485d5f8
Galen Strasen <galen@wearetmbr.com>
2017-04-29 21:23:33 -0700

    Semantic classes

--- wp-content/themes/rachelcole/template-replenish/page-template.php
+++ wp-content/themes/rachelcole/template-replenish/page-template.php
@@ -4,35 +4,42 @@ Template Name: Replenish Page
 */
 
 
-get_header('replenish');
-?>
+get_header('replenish'); ?>
+
+
+
+<header>
+
+  <?php
+
+  get_template_part('/template-replenish/partials/hero');
+  get_template_part('/template-replenish/partials/nav'); ?>
+
+</header>
+
 
 <main class="main">
 
   <?php
-    get_template_part('/template-replenish/partials/hero');
-    get_template_part('/template-replenish/partials/nav');
-    get_template_part('/template-replenish/partials/section1');
-    get_template_part('/template-replenish/partials/section2');
-    get_template_part('/template-replenish/partials/section3');
-    get_template_part('/template-replenish/partials/section4');
-    get_template_part('/template-replenish/partials/section5');
-    get_template_part('/template-replenish/partials/section6');
-    get_template_part('/template-replenish/partials/section7');
-    get_template_part('/template-replenish/partials/section8');
-    get_template_part('/template-replenish/partials/section9');
-    get_template_part('/template-replenish/partials/section10');
-    get_template_part('/template-replenish/partials/section11');
-    get_template_part('/template-replenish/partials/section12');
-    get_template_part('/template-replenish/partials/section13');
-    get_template_part('/template-replenish/partials/section14');
-    get_template_part('/template-replenish/partials/section15');
-    get_template_part('/template-replenish/partials/footer');
-  ?>
+  get_template_part('/template-replenish/partials/section1');
+  get_template_part('/template-replenish/partials/section2');
+  get_template_part('/template-replenish/partials/section3');
+  get_template_part('/template-replenish/partials/section4');
+  get_template_part('/template-replenish/partials/section5');
+  get_template_part('/template-replenish/partials/section6');
+  get_template_part('/template-replenish/partials/section7');
+  get_template_part('/template-replenish/partials/section8');
+  get_template_part('/template-replenish/partials/section9');
+  get_template_part('/template-replenish/partials/section10');
+  get_template_part('/template-replenish/partials/section11');
+  get_template_part('/template-replenish/partials/section12');
+  get_template_part('/template-replenish/partials/section13');
+  get_template_part('/template-replenish/partials/section14');
+  get_template_part('/template-replenish/partials/section15'); ?>
 
 </main>
 
 
-
-
-<?php get_footer('replenish');
+  <?php
+  get_template_part('/template-replenish/partials/footer');
+  get_footer('replenish');
