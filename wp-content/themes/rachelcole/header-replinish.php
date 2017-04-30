735baee6f53cd32c30d7ba5f8957f58e456f7c82
Galen Strasen <galen@wearetmbr.com>
2017-04-29 20:11:38 -0700

    Stage1 header cleanup without breaking site

--- wp-content/themes/rachelcole/header-replinish.php
+++ wp-content/themes/rachelcole/header-replinish.php
@@ -1,93 +1,30 @@
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?>>
 
-<!DOCTYPE html>
-<!-- saved from url=(0017)http://feast.dev/ -->
-<html class="no-js wf-inactive" lang="en-US" prefix="og: http://ogp.me/ns#"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
+<head>
+  <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Feast - Become a Well-fed Woman in 2017</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="http://feast.dev/wp-content/themes/rachelcole/assets/img/favicon.ico">
-  <link rel="alternate" type="application/rss+xml" title="Feast 2015 Feed" href="http://feast.dev/feed/">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php global $page, $paged; wp_title( '|', true, 'right' ); bloginfo( 'name' ); $site_description = get_bloginfo( 'description', 'display' ); if ( $site_description && ( is_home() || is_front_page() ) ) echo " | $site_description"; ?></title>
 
-  <!-- Typekit -->
+  <!-- TYPEKIT -->
+  <script src="//use.typekit.net/lug2lpk.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
 
-      <script src="//use.typekit.net/lug2lpk.js"></script>
-<script>try{Typekit.load();}catch(e){}</script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
-<!-- This site is optimized with the Yoast WordPress SEO plugin v1.7 - https://yoast.com/wordpress/plugins/seo/ -->
-<meta name="description" content="Find peace with food and begin to feast on your life - a masterclass from Rachel W Cole.">
-<link rel="canonical" href="http://feast.dev/">
-<meta property="og:locale" content="en_US">
-<meta property="og:type" content="website">
-<meta property="og:title" content="Feast - Become a Well-fed Woman in 2017">
-<meta property="og:description" content="FEAST is an intensive 3-month masterclass for women who want to come home to themselves, make peace with food, and feast on their lives.">
-<meta property="og:url" content="http://feast.dev/">
-<meta property="og:site_name" content="Feast 2015">
-<meta property="og:image" content="http://feast.rachelwcole.com/wp-content/uploads/2014/11/interest_homepage-image.jpg">
-<script type="application/ld+json">{ "@context": "http://schema.org", "@type": "WebSite", "url": "http://feast.dev/", "potentialAction": { "@type": "SearchAction", "target": "http://feast.dev/?s={search_term}", "query-input": "required name=search_term" } }</script>
-<!-- / Yoast WordPress SEO plugin. -->
+  <?php //wp_head(); ?>
 
-<link rel="alternate" type="application/rss+xml" title="Feast 2015 » Feast Sign Up Comments Feed" href="http://feast.dev/signup/feed/">
-<!-- <link rel="stylesheet" id="roots_css-css" href="<?php bloginfo('template_url')?>/template-replinish/assets/temp-files/main.min.css" type="text/css" media="all"> -->
-<link rel="stylesheet" id="fancybox-css" href="<?php bloginfo('template_url')?>/template-replinish/assets/temp-files/jquery.fancybox-1.3.7.min.css" type="text/css" media="screen">
-<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://feast.dev/xmlrpc.php?rsd">
-<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://feast.dev/wp-includes/wlwmanifest.xml">
-<meta name="generator" content="WordPress 4.0.17">
-<link rel="shortlink" href="http://feast.dev/">
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-21603057-1']);
-_gaq.push(['_trackPageview']);
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-</script>
-  <style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
 
-<!-- Easy FancyBox 1.5.7 using FancyBox 1.3.7 - RavanH (http://status301.net/wordpress-plugins/easy-fancybox/) -->
-<script type="text/javascript">
-/* <![CDATA[ */
-var fb_timeout = null;
-var fb_opts = { 'overlayShow' : true, 'hideOnOverlayClick' : true, 'showCloseButton' : true, 'centerOnScroll' : true, 'enableEscapeButton' : true, 'autoScale' : true };
-var easy_fancybox_handler = function(){
-  /* IMG */
-  var fb_IMG_select = 'a[href*=".jpg"]:not(.nofancybox,.pin-it-button), area[href*=".jpg"]:not(.nofancybox), a[href*=".jpeg"]:not(.nofancybox,.pin-it-button), area[href*=".jpeg"]:not(.nofancybox), a[href*=".png"]:not(.nofancybox,.pin-it-button), area[href*=".png"]:not(.nofancybox)';
-  jQuery(fb_IMG_select).addClass('fancybox image');
-  var fb_IMG_sections = jQuery('div.gallery');
-  fb_IMG_sections.each(function() { jQuery(this).find(fb_IMG_select).attr('rel', 'gallery-' + fb_IMG_sections.index(this)); });
-  jQuery('a.fancybox, area.fancybox, li.fancybox a:not(li.nofancybox a)').fancybox( jQuery.extend({}, fb_opts, { 'type' : 'image', 'transitionIn' : 'elastic', 'easingIn' : 'easeOutBack', 'transitionOut' : 'elastic', 'easingOut' : 'easeInBack', 'opacity' : false, 'hideOnContentClick' : false, 'titleShow' : true, 'titlePosition' : 'over', 'titleFromAlt' : true, 'showNavArrows' : true, 'enableKeyboardNav' : true, 'cyclic' : false }) );
-  /* Inline */
-  jQuery('a.fancybox-inline, area.fancybox-inline, li.fancybox-inline a').fancybox( jQuery.extend({}, fb_opts, { 'type' : 'inline', 'autoDimensions' : true, 'scrolling' : 'no', 'easingIn' : 'easeOutBack', 'easingOut' : 'easeInBack', 'opacity' : false, 'hideOnContentClick' : false }) );
-  /* Auto-click */
-  jQuery('#fancybox-auto').trigger('click');
-}
-/* ]]> */
-</script>
-<style id="style-1-cropbar-clipper">/* Copyright 2014 Evernote Corporation. All rights reserved. */
-.en-markup-crop-options {
-    top: 18px !important;
-    left: 50% !important;
-    margin-left: -100px !important;
-    width: 200px !important;
-    border: 2px rgba(255,255,255,.38) solid !important;
-    border-radius: 4px !important;
-}
-
-.en-markup-crop-options div div:first-of-type {
-    margin-left: 0px !important;
-}
-</style>
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url')?>/template-replinish/assets/temp-files/new_feast.css" media="all">
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url')?>/template-replinish/assets/css/replinish.css" media="all">
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url')?>/template-replinish/assets/temp-files/new_feast.css" media="all">
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url')?>/template-replinish/assets/css/replinish.css" media="all">
     <script src="<?php bloginfo('template_url')?>/template-replinish/assets/temp-files/jquery.min.js"></script>
     <script src="<?php bloginfo('template_url')?>/template-replinish/assets/temp-files/main.js"></script>
-    <script src="<?php bloginfo('template_url')?>/template-replinish/assets/temp-files/jquery.bxslider.min.js"></script>
-    <link href="<?php bloginfo('template_url')?>/template-replinish/assets/temp-files/jquery.bxslider.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
 
-<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+
+    <!-- SLICK SLIDER -->
+  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+  <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
 <script type="text/javascript">
   $(document).ready(function(){
 
@@ -112,15 +49,3 @@ var easy_fancybox_handler = function(){
 
 </head>
 <body class="home page page-id-269 page-template page-template-template_feast_new-php" id="replinish">
-
-  <!--[if lt IE 8]>
-    <div class="alert alert-warning">
-      You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.    </div>
-  <![endif]-->
-
-
-
-
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
