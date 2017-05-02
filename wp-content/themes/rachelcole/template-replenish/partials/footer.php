cf466eeddb8ff3d9d1150ba15ade37f3b14cc8d3
Galen Strasen <galen@wearetmbr.com>
2017-05-01 18:27:39 -0700

    Feedback round 1

--- wp-content/themes/rachelcole/template-replenish/partials/footer.php
+++ wp-content/themes/rachelcole/template-replenish/partials/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer bg-img">
-  <div class="container-fluid">
+  <div class="container">
     <div class="row">
       <div class="col-xs-12"><?php // text_items_footer ... text ?>
       <?php
@@ -12,29 +12,27 @@
         <p><em>Replenish.</em></p>
 
       <a href="https://lq120.infusionsoft.com/app/storeFront/showCategoryPage?categoryId=19" target="_blank" class="btn btn-alt btn-lg">Reserve</a>
+
+
       <hr>
-      <img src="<?php bloginfo('template_url')?>/template-replenish/assets/img/image_logo-footer.png" class="logo" alt="Replenish" />
-
-      </div>
-    </div>
-
-  </div><!-- /container -->
-  <nav class="return-nav">
-    <div class="container-fluid">
       <div class="row">
-        <div class="col-xs-6">
+       <div class="col-xs-4">
         <a class="sol return-link" href="http://rachelwcole.com"> <h5>Return to rachelwcole.com</h5></a>
         </div>
-        <div class="col-xs-6">
-          <ul class="social">
+        <div class="col-xs-4">
+      <img src="<?php bloginfo('template_url')?>/template-replenish/assets/img/image_logo-footer.png" class="logo" alt="Replenish" /></div>
+      <div class="col-xs-4">
+        <ul class="social">
             <li><a href="https://www.facebook.com/feedyourlife" target="_blank"><img src="<?php bloginfo('template_url')?>/template-replenish/assets/img/icon_social-facebook.png" alt=""></a></li>
             <li><a href="https://twitter.com/rachelwcole" target="_blank"><img src="<?php bloginfo('template_url')?>/template-replenish/assets/img/icon_social-twitter.png" alt=""></a></li>
             <li><a href="http://pinterest.com/rachelwcole/" target="_blank"><img src="<?php bloginfo('template_url')?>/template-replenish/assets/img/icon_social-pinterest.png" alt=""></a></li>
             <li><a href="http://instagram.com/rachelwcole" target="_blank"><img src="<?php bloginfo('template_url')?>/template-replenish/assets/img/icon_social-instagram.png" alt=""></a></li>
           </ul>
-        </div>
+      </div>
 
       </div>
     </div>
-  </nav>
+
+  </div><!-- /container -->
+
 </footer>
