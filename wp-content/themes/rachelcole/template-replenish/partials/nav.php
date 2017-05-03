d1e8b47ce63eb1e95c8f4394bebb1f07869e88ad
Galen Strasen <galen@wearetmbr.com>
2017-05-02 18:54:49 -0700

    Reworked navbar markup to use flex box - closes #22

--- wp-content/themes/rachelcole/template-replenish/partials/nav.php
+++ wp-content/themes/rachelcole/template-replenish/partials/nav.php
@@ -1,28 +1,24 @@
 <!--  Add navbar-fixed-top on scroll to make nav sticky-->
-<nav class="replinish-nav js-nav navbar navbar-default bg-darken">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#center-nav">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+<nav class="replenish-nav js-nav bg-darken">
+  <div class="flex-wrap">
+    <div class="col-sm nav-brand">
+
       <a class="navbar-brand js-scroll-to" href="#top"><img src="<?php bloginfo('template_url')?>/template-replenish/assets/img/image_logo-nav.png" class="nav-logo" alt="Replenish"></a>
     </div>
 
-    <div class="collapse navbar-collapse" id="center-nav">
-      <ul class="nav navbar-nav page-nav">
+    <div class="main-nav col-mid">
+      <ul class="page-nav">
         <li><a class="js-scroll-to" href="#retreat">The Retreat</a></li>
         <li><a class="js-scroll-to" href="#schedule">Schedule</a></li>
         <li><a class="js-scroll-to" href="#details">Fees &AMP; Accommodations</a></li>
         <li><a class="js-scroll-to" href="#faqs">FAQs</a></li>
       </ul>
-
-        <ul class="nav navbar-nav navbar-right nav-apply">
+    </div>
+    <div class="apply-right col-sm">
+      <ul class="nav-apply">
         <li><a class="btn" href="https://lq120.infusionsoft.com/app/storeFront/showCategoryPage?categoryId=19" target="_blank">Reserve</a></li>
+      </ul>
 
-        </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
